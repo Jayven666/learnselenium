199b114e6a78c11269d74b5de0762b1859db3c48
jayven <410654975@qq.com>
2021-07-25 13:50:31 +0800

    改动v1.0

--- test/homework5_14.py
+++ test/homework5_14.py
@@ -1,15 +1,15 @@
 # # 京东-find_element_by_id通过id去定位某一个控件
-# # 1.从selenium库导入webriver模块
-# from selenium import webdriver
-# # 2.初始化webdriver类的一个对象命名为driver，操作chrome浏览器
-# driver = webdriver.Chrome()
-# # 3.调用get()方法打开url地址，对象名.方法名
-# driver.get("https://www.jd.com/")
-# # 4.最大化窗口
-# driver.maximize_window()
-# # 5.对象名.find_element_by_id("id属性值")，定位控件中的第1种方式；
-# # .click()方法实现点击的动作
-# driver.find_element_by_id("settleup").click()
+# 1.从selenium库导入webriver模块
+from selenium import webdriver
+# 2.初始化webdriver类的一个对象命名为driver，操作chrome浏览器
+driver = webdriver.Chrome()
+# 3.调用get()方法打开url地址，对象名.方法名
+driver.get("https://www.jd.com/")
+# 4.最大化窗口
+driver.maximize_window()
+# 5.对象名.find_element_by_id("id属性值")，定位控件中的第1种方式；
+# .click()方法实现点击的动作
+driver.find_element_by_id("settleup").click()
 
 # # b站-find_element_by_id通过id去定位某一个控件
 # # 1.从selenium库导入webriver模块
